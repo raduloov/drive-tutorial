b4d7b8e5da0625b7a1dc5acea30c63f3d00cb14a
raduloov <raduloov@gmail.com>
2025-03-27 15:57:25 +0200

    analytics added

--- next.config.js
+++ next.config.js
@@ -10,25 +10,6 @@ const config = {
     ignoreDuringBuilds: true,
   },
 
-  async rewrites() {
-    return [
-      {
-        source: "/ingest/static/:path*",
-        destination: "https://us-assets.i.posthog.com/static/:path*",
-      },
-      {
-        source: "/ingest/:path*",
-        destination: "https://us.i.posthog.com/:path*",
-      },
-      {
-        source: "/ingest/decide",
-        destination: "https://us.i.posthog.com/decide",
-      },
-    ];
-  },
-  // This is required to support PostHog trailing slash API requests
-  skipTrailingSlashRedirect: true,
-
   typescript: {
     ignoreBuildErrors: true,
   },
